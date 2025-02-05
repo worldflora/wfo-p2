57381de1993934aaf08aca11bf71fedcce5eef6b
Roger <rhyam@rbge.org.uk>
2025-02-05 11:46:25 +0000

    place holder text

--- www/about.php
+++ www/about.php
@@ -6,10 +6,19 @@ require_once('header.php');
 ?>
 
 <div class="container" style="margin-top: 4%;">
-    <h1>About the WFO</h1>
-    <p>Background to the project</p>
+    <h1 style="margin-bottom: 0.5em;">About us</h1>
+    
+    <p>[This is place holder text for demo. Imagine something prettier!]</p>
+    
+    <div class="card shadow-sm bg-secondary-subtle" style="width: 100%">
+    <div class="card-header">
+        <h2>Mission</h2>
+    </div>
+    <div class="card-body bg-white">
+        <p>World Flora Online is the international initiative to achieve Target 1 of the Global Strategy for Plant Conservation and provides a global overview of the diversity of plant species. It is the essential tool for conservation planners, policymakers and practitioners at all levels.</p>
+    </div>
+    </div>
 </div>
-
 <?php
 require_once('footer.php');
 ?>
--- www/contribute.php
+++ www/contribute.php
@@ -5,11 +5,36 @@ $page_title = "WFO: Contributing";
 require_once('header.php');
 ?>
 
-<div class="container" style="margin-top: 4%;">
-    <h1>Contributing</h1>
-    <p>How to join a TEN etc</p>
-</div>
 
+<div class="container" style="margin-top: 4%;">
+    <h1 style="margin-bottom: 0.5em;">Contributing</h1>
+
+    <p>[This is place holder text for demo. Imagine something prettier!]</p>
+    
+    <div class="card shadow-sm bg-secondary-subtle" style="width: 100%">
+    <div class="card-header">
+        <h2>Taxonomic Expert Networks (TENS)</h2>
+    </div>
+    <div class="card-body bg-white">
+        <p>TENs are collaborations of taxonomic experts who work together to agree upon a consensus classification for their plant groups. TENs are the preferred approach for empowering the taxonomic community to contribute to a global checklist of plants as community-based consensus classifications are more robust and more sustainable than those produced by individuals.</p>
+        <p>TENs are encouraged to work at the family level, but we recognize that in some cases a different taxonomic rank may be more appropriate. Above the rank of family, we have ‘major group’ TENs (e.g. bryophytes, pteridophytes and conifers) and order-level TENs (e.g. Caryophyllales, Commelinales and Dipsacales). We also have a few suprageneric TENs (e.g. Boraginaceae subtribe Amsinckiinae and Papaveraceae tribe Papaverae), although these are seen as ‘stepping stones’ that will later become part of a family-level TEN.</p>
+        <p>A TEN is responsible for curating all names in their taxonomic group, however, we acknowledge that for most TENs this will be work in progress, with TENs providing incremental updates focussed on the taxa being actively worked on, whist other taxa await taxonomic scrutiny.</p>
+        <p>We ask TENs to be open, inclusive, representative and collaborative. They should include specialists from across the geographic distribution of the plant group, cover a range of taxonomic viewpoints, and be open to new membership.</p>
+    </div>
+    </div>
+
+    <div class="card shadow-sm bg-secondary-subtle" style="width: 100%">
+    <div class="card-header">
+        <h2>Domain Expert Networks (DENS)</h2>
+    </div>
+    <div class="card-body bg-white">
+        <p>Could we have something like this? Or call them something different?</p>
+    </div>
+    </div>
+
+
+
+</div>
 <?php
 require_once('footer.php');
 ?>
